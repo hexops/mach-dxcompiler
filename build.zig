d2e85032ea3f0a7de57c193b880c875e2c50301a
Stephen Gutekanst <stephen@hexops.com>
2023-10-31 19:18:43 -0700

    add from-source repository cloning logic
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -4,14 +4,7 @@ const Build = std.Build;
 pub fn build(b: *Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
-    const from_source = b.option(bool, "from_source", "Build DXC from source (large C++ codebase)");
-
-    const options = Options{
-        .install_libs = true,
-        .from_source = from_source,
-        .debug = true,
-    };
-    _ = options;
+    const from_source = b.option(bool, "from-source", "Build DXC from source (large C++ codebase)") orelse false;
 
     const mach_dxcompiler = b.addModule("mach-dxcompiler", .{
         .source_file = .{ .path = "src/main.zig" },
@@ -42,6 +35,10 @@ pub const Options = struct {
 
     /// The binary release version to use from https://github.com/hexops/mach-dxcompiler/releases
     binary_version: []const u8 = "release-d7b308b",
+
+    /// When building from source, which repository and revision to clone.
+    source_repository: []const u8 = "https://github.com/hexops/DirectXShaderCompiler",
+    source_revision: []const u8 = "a4b15ef762c19cb9b3e7fd92b562d458e9ae89e2", // main branch
 };
 
 pub fn link(b: *Build, step: *std.build.CompileStep, options: Options) !void {
@@ -54,9 +51,8 @@ pub fn link(b: *Build, step: *std.build.CompileStep, options: Options) !void {
 }
 
 fn linkFromSource(b: *Build, step: *std.build.CompileStep, options: Options) !void {
-    _ = b;
-    _ = options;
     _ = step;
+    try ensureGitRepoCloned(b.allocator, options.source_repository, options.source_revision, sdkPath("/libs/DirectXShaderCompiler"));
 
     // TODO
 }
@@ -66,4 +62,84 @@ pub fn linkFromBinary(b: *Build, step: *std.build.CompileStep, options: Options)
     _ = step;
     _ = b;
     // TODO
+}
+
+fn ensureGitRepoCloned(allocator: std.mem.Allocator, clone_url: []const u8, revision: []const u8, dir: []const u8) !void {
+    if (isEnvVarTruthy(allocator, "NO_ENSURE_SUBMODULES") or isEnvVarTruthy(allocator, "NO_ENSURE_GIT")) {
+        return;
+    }
+
+    ensureGit(allocator);
+
+    if (std.fs.openDirAbsolute(dir, .{})) |_| {
+        const current_revision = try getCurrentGitRevision(allocator, dir);
+        if (!std.mem.eql(u8, current_revision, revision)) {
+            // Reset to the desired revision
+            exec(allocator, &[_][]const u8{ "git", "fetch" }, dir) catch |err| std.debug.print("warning: failed to 'git fetch' in {s}: {s}\n", .{ dir, @errorName(err) });
+            try exec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
+            try exec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
+        }
+        return;
+    } else |err| return switch (err) {
+        error.FileNotFound => {
+            std.log.info("cloning required dependency..\ngit clone {s} {s}..\n", .{ clone_url, dir });
+
+            try exec(allocator, &[_][]const u8{ "git", "clone", "-c", "core.longpaths=true", clone_url, dir }, sdkPath("/"));
+            try exec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
+            try exec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
+            return;
+        },
+        else => err,
+    };
+}
+
+fn getCurrentGitRevision(allocator: std.mem.Allocator, cwd: []const u8) ![]const u8 {
+    const result = try std.ChildProcess.exec(.{ .allocator = allocator, .argv = &.{ "git", "rev-parse", "HEAD" }, .cwd = cwd });
+    allocator.free(result.stderr);
+    if (result.stdout.len > 0) return result.stdout[0 .. result.stdout.len - 1]; // trim newline
+    return result.stdout;
+}
+
+fn ensureGit(allocator: std.mem.Allocator) void {
+    const argv = &[_][]const u8{ "git", "--version" };
+    const result = std.ChildProcess.exec(.{
+        .allocator = allocator,
+        .argv = argv,
+        .cwd = ".",
+    }) catch { // e.g. FileNotFound
+        std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
+        std.process.exit(1);
+    };
+    defer {
+        allocator.free(result.stderr);
+        allocator.free(result.stdout);
+    }
+    if (result.term.Exited != 0) {
+        std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
+        std.process.exit(1);
+    }
+}
+
+fn exec(allocator: std.mem.Allocator, argv: []const []const u8, cwd: []const u8) !void {
+    var child = std.ChildProcess.init(argv, allocator);
+    child.cwd = cwd;
+    _ = try child.spawnAndWait();
+}
+
+fn isEnvVarTruthy(allocator: std.mem.Allocator, name: []const u8) bool {
+    if (std.process.getEnvVarOwned(allocator, name)) |truthy| {
+        defer allocator.free(truthy);
+        if (std.mem.eql(u8, truthy, "true")) return true;
+        return false;
+    } else |_| {
+        return false;
+    }
+}
+
+fn sdkPath(comptime suffix: []const u8) []const u8 {
+    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
+    return comptime blk: {
+        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
+        break :blk root_dir ++ suffix;
+    };
 }
